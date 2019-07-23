5b46e4758d34898f9b70b234baf2108051dccfbe
enan <enan332@gmail.com>
2019-07-23 12:33:21 -0500

    init commit, add linreg and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+#Google's tensorflow code but sexier and good
+Here's a wrapper for Google'd ML Crash course where it is easy to edit and work with
---
+++ linear_regresion.py
@@ -0,0 +1,273 @@
+from __future__ import print_function
+"""# First Steps with TensorFlow
+
+The [data](https://developers.google.com/machine-learning/crash-course/california-housing-data-description) is based on 1990 census data from California.
+
+## Setup
+In this first cell, we'll load the necessary libraries.
+"""
+
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+
+dataset_main = pd.read_csv("https://enan.rocks/linregTD.csv", sep=",")
+
+linreg_y = "val1"
+linreg_x = "val2"
+cSteps = 100
+cPeriods = 50
+cSampleSize = 300
+
+
+
+
+dataset_main = dataset_main.reindex(np.random.permutation(dataset_main.index))
+dataset_main[linreg_y] /= 1000.0
+dataset_main
+
+
+
+dataset_main.describe()
+
+
+# Define the input feature: total_rooms.
+my_feature = dataset_main[[linreg_x]]
+
+# Configure a numeric feature column for total_rooms.
+feature_columns = [tf.feature_column.numeric_column(linreg_x)]
+
+
+
+# Define the label.
+targets = dataset_main[linreg_y]
+
+
+# Use gradient descent as the optimizer for training the model.
+my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.0000001)
+my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+
+# Configure the linear regression model with our feature columns and optimizer.
+# Set a learning rate of 0.0000001 for Gradient Descent.
+linear_regressor = tf.estimator.LinearRegressor(
+    feature_columns=feature_columns,
+    optimizer=my_optimizer
+)
+
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model of one feature.
+  
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+  
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}                                           
+ 
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    
+    # Shuffle the data, if specified.
+    if shuffle:
+      ds = ds.shuffle(buffer_size=10000)
+    
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+linear_regressor.train(		
+  input_fn = lambda:my_input_fn(my_feature, targets),		
+  steps= cSteps		
+)
+
+
+# Create an input function for predictions.
+# Note: Since we're making just one prediction for each example, we don't 
+# need to repeat or shuffle the data here.
+prediction_input_fn =lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
+
+# Call predict() on the linear_regressor to make predictions.
+predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+
+# Format predictions as a NumPy array, so we can calculate error metrics.
+predictions = np.array([item['predictions'][0] for item in predictions])
+
+# Print Mean Squared Error and Root Mean Squared Error.
+mean_squared_error = metrics.mean_squared_error(predictions, targets)
+root_mean_squared_error = math.sqrt(mean_squared_error)
+print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
+print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
+
+
+min_house_value = dataset_main[linreg_y].min()
+max_house_value = dataset_main[linreg_y].max()
+min_max_difference = max_house_value - min_house_value
+
+print("Min. Median House Value: %0.3f" % min_house_value)
+print("Max. Median House Value: %0.3f" % max_house_value)
+print("Difference between Min. and Max.: %0.3f" % min_max_difference)
+print("Root Mean Squared Error: %0.3f" % root_mean_squared_error)
+
+
+calibration_data = pd.DataFrame()
+calibration_data["predictions"] = pd.Series(predictions)
+calibration_data["targets"] = pd.Series(targets)
+calibration_data.describe()
+
+
+sample = dataset_main.sample(n=300)
+
+
+# Get the min and max total_rooms values.
+x_0 = sample[linreg_x].min()
+x_1 = sample[linreg_x].max()
+
+# Retrieve the final weight and bias generated during training.
+weight = linear_regressor.get_variable_value('linear/linear_model/'+linreg_x+'/weights')[0]
+bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+
+# Get the predicted linreg_ys for the min and max total_rooms values.
+y_0 = weight * x_0 + bias 
+y_1 = weight * x_1 + bias
+
+# Plot our regression line from (x_0, y_0) to (x_1, y_1).
+plt.plot([x_0, x_1], [y_0, y_1], c='r')
+
+# Label the graph axes.
+plt.ylabel(linreg_y)
+plt.xlabel(linreg_x)
+
+# Plot a scatter plot from our data sample.
+plt.scatter(sample[linreg_x], sample[linreg_y])
+
+# Display graph.
+plt.show()
+
+
+def train_model(learning_rate, steps, batch_size, input_feature=linreg_x):
+  """Trains a linear regression model of one feature.
+  
+  Args:
+    learning_rate: A `float`, the learning rate.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    input_feature: A `string` specifying a column from `dataset_main`
+      to use as input feature.
+  """
+  
+  periods = cPeriods
+  steps_per_period = steps / periods
+
+  my_feature = input_feature
+  my_feature_data = dataset_main[[my_feature]]
+  my_label = linreg_y
+  targets = dataset_main[my_label]
+
+  # Create feature columns.
+  feature_columns = [tf.feature_column.numeric_column(my_feature)]
+  
+  # Create input functions.
+  training_input_fn = lambda:my_input_fn(my_feature_data, targets, batch_size=batch_size)
+  prediction_input_fn = lambda: my_input_fn(my_feature_data, targets, num_epochs=1, shuffle=False)
+  
+  # Create a linear regressor object.
+  my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  linear_regressor = tf.estimator.LinearRegressor(
+      feature_columns=feature_columns,
+      optimizer=my_optimizer
+  )
+
+  # Set up to plot the state of our model's line each period.
+  plt.figure(figsize=(15, 6))
+  plt.subplot(1, 2, 1)
+  plt.title("Learned Line by Period")
+  plt.ylabel(my_label)
+  plt.xlabel(my_feature)
+  sample = dataset_main.sample(n=cSampleSize)
+  plt.scatter(sample[my_feature], sample[my_label])
+  colors = [cm.coolwarm(x) for x in np.linspace(-1, 1, periods)]
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("RMSE (on training data):")
+  root_mean_squared_errors = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    linear_regressor.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period
+    )
+    # Take a break and compute predictions.
+    predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+    predictions = np.array([item['predictions'][0] for item in predictions])
+    
+    # Compute loss.
+    root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(predictions, targets))
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, root_mean_squared_error))
+    # Add the loss metrics from this period to our list.
+    root_mean_squared_errors.append(root_mean_squared_error)
+    # Finally, track the weights and biases over time.
+    # Apply some math to ensure that the data and line are plotted neatly.
+    y_extents = np.array([0, sample[my_label].max()])
+    
+    weight = linear_regressor.get_variable_value('linear/linear_model/%s/weights' % input_feature)[0]
+    bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+
+    x_extents = (y_extents - bias) / weight
+    x_extents = np.maximum(np.minimum(x_extents,
+                                      sample[my_feature].max()),
+                           sample[my_feature].min())
+    y_extents = weight * x_extents + bias
+    plt.plot(x_extents, y_extents, color=colors[period]) 
+  print("Model training finished.")
+
+  # Output a graph of loss metrics over periods.
+  plt.subplot(1, 2, 2)
+  plt.ylabel('RMSE')
+  plt.xlabel('Periods')
+  plt.title("Root Mean Squared Error vs. Periods")
+  plt.tight_layout()
+  plt.plot(root_mean_squared_errors)
+
+  # Output a table with calibration data.
+  calibration_data = pd.DataFrame()
+  calibration_data["predictions"] = pd.Series(predictions)
+  calibration_data["targets"] = pd.Series(targets)
+  display.display(calibration_data.describe())
+
+  print("Final RMSE (on training data): %0.2f" % root_mean_squared_error)
+
+
+
+train_model(
+    learning_rate=0.00002,
+    steps=100,
+    batch_size=5
+)
